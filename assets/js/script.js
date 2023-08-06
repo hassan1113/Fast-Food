66af8f7086ebba257beefa650738a8e2ee144145
hassan1113 <108580176+hassan1113@users.noreply.github.com>
2023-08-06 13:14:45 +0500

    added assests

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// Custom Script
+
+$(document).ready(function(){
+    // Owl Carousel
+    $("#banner-slider .owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        nav: true,
+    });
+
+});
